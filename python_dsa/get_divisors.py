0d2aca5413632f95e1b3f80fd48f23b84a5ccb99
Rajesh Kumar <rkumar.python@gmail.com>
2026-02-08 23:55:04 +0530

    gettting divisiors

--- python_dsa/get_divisors.py
+++ python_dsa/get_divisors.py
@@ -1,6 +1,29 @@
 # Print the Factors or Divisors of a given number
-10 = [1, 2, 5, 10]
-15 = [1, 3, 15]
-25 = [1, 5, 25]
-7 = [1, 7]
-19 = [1, 19]
+# 10 = [1, 2, 5, 10]
+# 15 = [1, 3, 15]
+# 25 = [1, 5, 25]
+# 7 = [1, 7]
+# 19 = [1, 19]
+
+from math import sqrt
+
+# version 1
+def get_factors(number):
+    factors = [1]
+    for num in range(2, number + 1):
+        if number % num == 0:
+            factors.append(num)
+    return factors
+
+
+if __name__ == "__main__":
+    number = 15
+    print(get_factors(number))
+
+
+# version 2 --> using sqrt function
+
+
+# Understands 
+# 1. Hashmap concept
+# 2. Recursion concept
