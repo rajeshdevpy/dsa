a7ef1e9bdb94981ae691300956438af16e19e698
Rajesh Kumar <rkumar.python@gmail.com>
2026-02-13 21:29:51 +0530

    Added logic for finding largest elemnet from a list of numbers

---
+++ python_dsa/largest_element.py
@@ -0,0 +1,9 @@
+nums = [-33, 20, 1, 55, 66, 76, 12]
+def get_longest(nums):
+    largest_num = nums[0]
+    for num in nums:
+        # if num > largest_num:
+        #     largest_num = num
+        largest_num = max(largest_num, num)
+    return largest_num
+print(get_longest(nums))
