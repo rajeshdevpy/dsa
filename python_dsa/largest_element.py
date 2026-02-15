774819a35a1d81a7ddd0656724c9e939cdf1163d
Rajesh Kumar <rkumar.python@gmail.com>
2026-02-16 00:04:50 +0530

    day 1 memory model

--- python_dsa/largest_element.py
+++ python_dsa/largest_element.py
@@ -1,6 +1,7 @@
 nums = [-33, 20, 1, 55, 66, 76, 12]
 def get_longest(nums):
     largest_num = nums[0]
+    largest_num = float("-inf")
     for num in nums:
         # if num > largest_num:
         #     largest_num = num
